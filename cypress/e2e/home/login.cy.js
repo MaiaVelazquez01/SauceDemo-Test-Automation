87b6ee2bc003e3d283f4d18ddd55b59ea3bff809
unknown <mvelazquezyarbe@gmail.com>
2026-05-31 03:22:44 +0200

    test: automate TC-2.0 login with incorrect password

--- cypress/e2e/home/login.cy.js
+++ cypress/e2e/home/login.cy.js
@@ -12,4 +12,13 @@ describe('Login Sauce Demo', () => {
         cy.get('.app_logo').should('have.text','Swag Labs')
     })
 
+    it('Login con contraseña incorrecta', () => {
+        cy.get('[data-test="username"]').type('standard_user')
+        cy.get('[data-test="password"]').type('wrong_pass')
+        cy.get('[data-test="login-button"]').click()
+        cy.get('[data-test="error"]')
+        .should('be.visible')
+        .and('contain','Epic sadface: Username and password do not match any user in this service')
+    })
+
 })
