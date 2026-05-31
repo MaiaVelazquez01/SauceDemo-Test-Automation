2088ff561254d7a0c40aa452a02f3899c2861574
unknown <mvelazquezyarbe@gmail.com>
2026-05-31 23:26:15 +0200

    test: automate TC-4.0 login with locked out user

--- cypress/e2e/home/login.cy.js
+++ cypress/e2e/home/login.cy.js
@@ -28,4 +28,13 @@ describe('Login Sauce Demo', () => {
         .and('contain','Epic sadface: Username is required')
     })
 
+    it('Login con usuario bloqueado (locked_out_user)', () => {
+        cy.get('[data-test="username"]').type('locked_out_user')
+        cy.get('[data-test="password"]').type('secret_sauce')
+        cy.get('[data-test="login-button"]').click()
+        cy.get('[data-test="error"]')
+        .should('be.visible')
+        .and('contain','Epic sadface: Sorry, this user has been locked out.')
+    })
+
 })
